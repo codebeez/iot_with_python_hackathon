6a8fe63f4130aad4f9763031716fbaa1f18b38e6
Erencan Kanmaz <erencankanmaz111@gmail.com>
2024-04-10 11:39:11 +0200

    ex 1-2

--- Exercise 1/blink_example.py
+++ Exercise 1/blink_example.py
@@ -1,13 +1,16 @@
 from machine import Pin
 from utime import sleep
 
+pin = Pin(15, Pin.OUT)
+pin2 = Pin("LED", Pin.OUT)
 
-pin = Pin("LED", Pin.OUT)
+pin2.toggle()
 
 print("LED starts flashing...")
 while True:
     try:
         pin.toggle()
+        pin2.toggle()
         sleep(1)  # sleep 1sec
     except KeyboardInterrupt:
         break
--- Exercise 2/morse.py
+++ Exercise 2/morse.py
@@ -1,1 +1,69 @@
 # Implement here
+from machine import PWM
+from utime import sleep
+
+MORSE_CODE_DICT = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": " ",
+}
+
+BASE_DELAY = 0.2
+MORSE_DELAY_DICT = {".": 1, "-": 3, "intra": 1, " ": 3, "end": 7}
+
+buzzer = PWM(15)
+buzzer.freq(131)
+
+morse_word = "SOS"
+morse_code = " ".join([MORSE_CODE_DICT[char] for char in morse_word])
+
+
+while True:
+    try:
+        for char in morse_code:
+            if char in [".", "-"]:
+                buzzer.duty_u16(1000)
+                sleep(BASE_DELAY * MORSE_DELAY_DICT[char])
+                buzzer.duty_u16(0)
+                sleep(BASE_DELAY * MORSE_DELAY_DICT["intra"])
+            else:
+                buzzer.duty_u16(0)
+                sleep(BASE_DELAY * (MORSE_DELAY_DICT[char] - MORSE_DELAY_DICT["intra"]))
+        sleep(BASE_DELAY * (MORSE_DELAY_DICT["end"] - MORSE_DELAY_DICT["intra"]))
+    except KeyboardInterrupt:
+        break
+buzzer.duty_u16(0)
